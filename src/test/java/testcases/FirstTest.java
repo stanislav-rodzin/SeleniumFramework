9fccbe8c31c9c9f4a77d910a14ec29b9c586cac2
stanislav-rodzin <138123829+stanislav-rodzin@users.noreply.github.com>
2023-06-29 14:37:52 +0200

    first test

---
+++ src/test/java/testcases/FirstTest.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+
+    @Test
+    public void startDriver(){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\stani\\IdeaProjects\\SeleniumFramework\\src\\test\\resources\\executables\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://mvnrepository.com/artifact/org.testng/testng/7.1.0");
+    }
+}
